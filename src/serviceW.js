816e00a1bc479417165e1f7d306d004580ae7523
sahoodcom <sahoodhkr@gmail.com>
2022-07-20 10:00:32 +0530

    ./-  removed dot

--- src/serviceW.js
+++ src/serviceW.js
@@ -1,6 +1,6 @@
 export default function serviceW()
 {
-    let swUrl=  `${process.env.PUBLIC_URL}./serviceWDev.js`
+    let swUrl=  `${process.env.PUBLIC_URL}/serviceWDev.js`
     navigator.serviceWorker.register(swUrl).then((response)=>{
        
     }) 
